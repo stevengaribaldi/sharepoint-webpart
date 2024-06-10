28e50567e9c38db9b0f78c1a53278aedf8f07085
AcevedoEsteban <astevenaa@gmail.com>
2024-06-10 15:59:28 -0700

    [fe] feat: log document data and current user email in Demo component
    Added logging of document data to the console in the Demo

--- src/webparts/demo/DemoWebPart.ts
+++ src/webparts/demo/DemoWebPart.ts
@@ -1,139 +1,3 @@
-// //sharepoint webpart class
-// import * as React from 'react';
-// import * as ReactDom from 'react-dom';
-// import { Version } from '@microsoft/sp-core-library';
-
-// import { getSP } from './pnpjsConfig';
-// import {
-//   type IPropertyPaneConfiguration,
-//   PropertyPaneTextField
-// } from '@microsoft/sp-property-pane';
-// import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-// import { IReadonlyTheme } from '@microsoft/sp-component-base';
-
-// import * as strings from 'DemoWebPartStrings';
-// import Demo from './components/Demo';
-// import { IDemoProps } from './components/IDemoProps';
-
-// export interface IDemoWebPartProps {
-//   description: string;
-// }
-
-// export default class DemoWebPart extends BaseClientSideWebPart<IDemoWebPartProps> {
-
-//   private _isDarkTheme: boolean = false;
-//   private _environmentMessage: string = '';
-
-
-//   public async onInit(): Promise<void> {
-//   try {
-//     this._environmentMessage = await this._getEnvironmentMessage();
-
-//     await super.onInit();
-
-//     //Initialize our _sp object that we can then use in other packages without having to pass around the context.
-//     // Check out pnpjsConfig.ts for an example of a project setup file.
-// //this is are sharepoint context
-//     getSP(this.context);
-//   } catch (error) {
-//     console.error("Error during initialization:", error);
-//   }
-// }
-
-
-//   public render(): void {
-//     const element: React.ReactElement<IDemoProps> = React.createElement(
-//       Demo,
-//       {
-//         description: this.properties.description,
-//         isDarkTheme: this._isDarkTheme,
-//         environmentMessage: this._environmentMessage,
-//         hasTeamsContext: !!this.context.sdks.microsoftTeams,
-//         userDisplayName: this.context.pageContext.user.displayName
-//       }
-//     );
-
-//     ReactDom.render(element, this.domElement);
-//   }
-
-
-
-//   private _getEnvironmentMessage(): Promise<string> {
-//     if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
-//       return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
-//         .then(context => {
-//           let environmentMessage: string = '';
-//           switch (context.app.host.name) {
-//             case 'Office': // running in Office
-//               environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
-//               break;
-//             case 'Outlook': // running in Outlook
-//               environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
-//               break;
-//             case 'Teams': // running in Teams
-//             case 'TeamsModern':
-//               environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
-//               break;
-//             default:
-//               environmentMessage = strings.UnknownEnvironment;
-//           }
-
-//           return environmentMessage;
-//         });
-//     }
-
-//     return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
-//   }
-
-//   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
-//     if (!currentTheme) {
-//       return;
-//     }
-
-//     this._isDarkTheme = !!currentTheme.isInverted;
-//     const {
-//       semanticColors
-//     } = currentTheme;
-
-//     if (semanticColors) {
-//       this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
-//       this.domElement.style.setProperty('--link', semanticColors.link || null);
-//       this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
-//     }
-
-//   }
-
-//   protected onDispose(): void {
-//     ReactDom.unmountComponentAtNode(this.domElement);
-//   }
-
-//   protected get dataVersion(): Version {
-//     return Version.parse('1.0');
-//   }
-
-//   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-//     return {
-//       pages: [
-//         {
-//           header: {
-//             description: strings.PropertyPaneDescription
-//           },
-//           groups: [
-//             {
-//               groupName: strings.BasicGroupName,
-//               groupFields: [
-//                 PropertyPaneTextField('description', {
-//                   label: strings.DescriptionFieldLabel
-//                 })
-//               ]
-//             }
-//           ]
-//         }
-//       ]
-//     };
-//   }
-// }
-
 
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
@@ -144,11 +8,9 @@ import {
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
 
-
 import * as strings from 'DemoWebPartStrings';
 import Demo from './components/Demo';
 import { IAsyncAwaitPnPJsProps } from './components/Demo';
-
 
 import { spfi, SPFI, SPFx } from "@pnp/sp";
 
@@ -159,10 +21,8 @@ export interface IDemoWebPartProps {
 export default class DemoWebPart extends BaseClientSideWebPart<IDemoWebPartProps> {
   private sp: SPFI;
 
-  // // https://github.com/SharePoint/PnP-JS-Core/wiki/Using-sp-pnp-js-in-SharePoint-Framework
   public async onInit(): Promise<void> {
     await super.onInit();
-
     this.sp = spfi().using(SPFx(this.context));
   }
 
@@ -176,6 +36,10 @@ export default class DemoWebPart extends BaseClientSideWebPart<IDemoWebPartProps
     );
 
     ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
   }
 
   protected get dataVersion(): Version {
@@ -204,4 +68,3 @@ export default class DemoWebPart extends BaseClientSideWebPart<IDemoWebPartProps
     };
   }
 }
-
--- src/webparts/demo/components/demoEx.tsx
+++ src/webparts/demo/components/demoEx.tsx
@@ -232,3 +232,216 @@
 //   }
 // }
 
+//
+//
+// // import * as React from "react";
+// import styles from "./Demo.module.scss";
+
+// import { IFile, IResponseItem } from "./interface";
+
+// import { SPFI } from "@pnp/sp";
+// import "@pnp/sp/webs";
+// import "@pnp/sp/lists";
+// import "@pnp/sp/items";
+
+// export interface IAsyncAwaitPnPJsProps {
+//   description: string;
+//   sp: SPFI;
+// }
+
+// export interface IAsyncAwaitPnPJsState {
+//   items: IFile[];
+//   errors: string[];
+// }
+
+// export default class Demo extends React.Component<IAsyncAwaitPnPJsProps, IAsyncAwaitPnPJsState> {
+
+//   constructor(props: IAsyncAwaitPnPJsProps) {
+//     super(props);
+//     // set initial state
+//     this.state = {
+//       items: [],
+//       errors: []
+//     };
+//   }
+
+//  componentDidMount(): void {
+//     this._readAllFilesSize("Documents").catch(error => console.error(error));
+//   }
+
+//   public render(): React.ReactElement<IAsyncAwaitPnPJsProps> {
+//     // calculate total of file sizes
+//     const totalDocs: number = this.state.items.length > 0
+//       ? this.state.items.reduce<number>((acc: number, item: IFile) => {
+//           return acc + Number(item.Size);
+//         }, 0)
+//       : 0;
+
+//     return (
+//       <div className={styles.demo}>
+//         <div className={"ms-Grid-row ms-bgColor-themeDark ms-fontColor-white " + styles.row}>
+//           <div className="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
+//             <span className="ms-font-xl ms-fontColor-white">Welcome to SharePoint Async Await SP PnP JS Demo!</span>
+//             <div>{this._getErrors()}</div>
+//             <p className="ms-font-l ms-fontColor-white">List of documents:</p>
+//             <div>
+//               <div className={styles.row}>
+//                 <div className={styles.left}>Name</div>
+//                 <div className={styles.right}>Size (KB)</div>
+//                 <div className={`${styles.clear} ${styles.header}`}></div>
+//               </div>
+//               {this.state.items.map((item, idx) => (
+//                 <div key={idx} className={styles.row}>
+//                   <div className={styles.left}>{item.Name}</div>
+//                   <div className={styles.right}>{(item.Size / 1024).toFixed(2)}</div>
+//                   <div className={styles.clear}></div>
+//                 </div>
+//               ))}
+//               <div className={styles.row}>
+//                 <div className={`${styles.clear} ${styles.header}`}></div>
+//                 <div className={styles.left}>Total: </div>
+//                 <div className={styles.right}>{(totalDocs / 1024).toFixed(2)}</div>
+//                 <div className={`${styles.clear} ${styles.header}`}></div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+
+//       </div>
+//     );
+//   }
+
+//   private _readAllFilesSize = async (libraryName: string): Promise<void> => {
+//     try {
+//       const response: IResponseItem[] = await this.props.sp.web.lists
+//         .getByTitle(libraryName)
+//         .items.select("Title", "FileLeafRef", "File/Length")
+//         .expand("File/Length")();
+
+//       // use map to convert IResponseItem[] into our internal object IFile[]
+//       const items: IFile[] = response.map((item: IResponseItem) => ({
+//         Id: item.Id,
+//         Title: item.Title,
+//         Size: item.File.Length,
+//         Name: item.FileLeafRef
+//       }));
+
+//       // set our Component´s State
+//       this.setState({ items });
+
+//       // intentionally set wrong query to see console errors...
+//       // const failResponse: IResponseItem[] = await this.props.sp.web.lists
+//       //   .getByTitle(libraryName)
+//       //   .items.select("Title", "FileLeafRef", "File/Length")
+//       //   .expand("File/Length")();
+//     } catch (error) {
+//       // set a new state conserving the previous state + the new error
+//       this.setState({ errors: [...this.state.errors, error] });
+//     }
+//   };
+
+//   private _getErrors() {
+//     return this.state.errors.length > 0 ? (
+//       <div style={{ color: "orangered" }}>
+//         <div>Errors:</div>
+//         {this.state.errors.map((item, idx) => (
+//           <div key={idx}>{JSON.stringify(item)}</div>
+//         ))}
+//       </div>
+//     ) : null;
+//   }
+// }
+
+// import * as React from "react";
+
+// import React, { useEffect, useState } from "react";
+// import styles from "./Demo.module.scss";
+
+// import { IFile, IResponseItem } from "./interface";
+// import { SPFI } from "@pnp/sp";
+// import "@pnp/sp/webs";
+// import "@pnp/sp/lists";
+// import "@pnp/sp/items";
+
+// export interface IAsyncAwaitPnPJsProps {
+//   description: string;
+//   sp: SPFI;
+// }
+
+// const Demo: React.FC<IAsyncAwaitPnPJsProps> = ({ description, sp }) => {
+//   const [items, setItems] = useState<IFile[]>([]);
+//   const [errors, setErrors] = useState<any[]>([]);
+
+//   useEffect(() => {
+//     const fetchData = async (libraryName: string): Promise<void> => {
+//       try {
+//         const response: IResponseItem[] = await sp.web.lists
+//           .getByTitle(libraryName)
+//           .items.select("Title", "FileLeafRef", "File/Length")
+//           .expand("File/Length")();
+
+//         const fetchedItems: IFile[] = response.map((item: IResponseItem) => ({
+//           Id: item.Id,
+//           Title: item.Title,
+//           Size: item.File.Length,
+//           Name: item.FileLeafRef
+//         }));
+
+//         setItems(fetchedItems);
+//       } catch (error) {
+//         setErrors(prevErrors => [...prevErrors, error]);
+//       }
+//     };
+
+//     void fetchData("Documents");
+//   }, [sp]);
+
+//   const totalDocs: number = items.length > 0
+//     ? items.reduce<number>((acc, item) => acc + Number(item.Size), 0)
+//     : 0;
+
+//   const renderErrors = () => (
+//     errors.length > 0 && (
+//       <div style={{ color: "orangered" }}>
+//         <div>Errors:</div>
+//         {errors.map((error, idx) => (
+//           <div key={idx}>{JSON.stringify(error)}</div>
+//         ))}
+//       </div>
+//     )
+//   );
+
+//   return (
+//     <div className={styles.demo}>
+//       <div className={`ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`}>
+//         <div className="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
+//           <span className="ms-font-xl ms-fontColor-white">Welcome to SharePoint Async Await SP PnP JS Demo!</span>
+//           {renderErrors()}
+//           <p className="ms-font-l ms-fontColor-white">List of documents:</p>
+//           <div>
+//             <div className={styles.row}>
+//               <div className={styles.left}>Name</div>
+//               <div className={styles.right}>Size (KB)</div>
+//               <div className={`${styles.clear} ${styles.header}`} />
+//             </div>
+//             {items.map((item, idx) => (
+//               <div key={idx} className={styles.row}>
+//                 <div className={styles.left}>{item.Name}</div>
+//                 <div className={styles.right}>{(item.Size / 1024).toFixed(2)}</div>
+//                 <div className={styles.clear} />
+//               </div>
+//             ))}
+//             <div className={styles.row}>
+//               <div className={`${styles.clear} ${styles.header}`} />
+//               <div className={styles.left}>Total: </div>
+//               <div className={styles.right}>{(totalDocs / 1024).toFixed(2)}</div>
+//               <div className={styles.clear} />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Demo;
